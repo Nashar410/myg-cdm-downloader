d1a35e41c04f9dfae957f695edf58d8c334b8545
JACQUENET Christophe <jjacquenet@myges.fr>
2021-11-21 21:45:54 +0100

    INIT

---
+++ script/install.sh
@@ -0,0 +1,3 @@
+#git clone https://github.com/Nashar410/myg-cdm-downloader
+#cd myg-cdm-downloader/lib
+#npm install
---
+++ script/update.sh
@@ -0,0 +1,7 @@
+cd ..
+git pull https://github.com/Nashar410/myg-cdm-downloader -f
+cd lib
+npm install
+echo "************************************************"
+echo "************* Mise à jour complète ! ***********"
+echo "************************************************"
